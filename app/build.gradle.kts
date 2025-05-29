4246618eae447f3772e25b7674c0db8d9d99ea87
PuttiCristian <cristianputti@gmail.com>
2025-05-29 11:42:25 +0200

    implementazione di moovie.xml e del moovie_adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,17 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/myrestapp_cristianputti/MainActivity.kt
+++ app/src/main/java/com/example/myrestapp_cristianputti/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.myrestapp_cristianputti
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.security.MessageDigest
+import java.util.Base64
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val userName = findViewById<EditText>(R.id.txtUserLoginActivity)
+        val userPass = findViewById<EditText>(R.id.textPswLoginActivity)
+        val btnAcc = findViewById<Button>(R.id.btnLoginActivity)
+
+        btnAcc.setOnClickListener {
+            val result = valida(userName.text.toString(), userPass.text.toString())
+
+            if (result != "Credenziali non valide") {
+                Toast.makeText(this, "Accesso riuscito!", Toast.LENGTH_SHORT).show()
+
+                // Passaggio all'activity Menu.kt
+                val intent = Intent(this, Menu::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    fun valida(nome: String, password: String): String {
+        if (nome.isNotBlank() && password.length > 5) {
+            if (nome == "Cristian" && password == "Puttone.2006") {
+                val digest = MessageDigest.getInstance("SHA-256")
+                val hashBytes = digest.digest(password.toByteArray(Charsets.UTF_8))
+                return Base64.getEncoder().encodeToString(hashBytes)
+            }
+        }
+        return "Credenziali non valide"
     }
 }
